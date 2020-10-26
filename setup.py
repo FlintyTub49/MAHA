83393d8b7a1e910b0947b12d3e5e28f48921d7bb
Arth Akhouri <64348340+FlintyTub49@users.noreply.github.com>
2020-10-27 00:12:06 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from distutils.core import setup
+setup(
+  name = 'MAHA',
+  packages = ['MAHA'],
+  version = '0.1',
+  license='MIT',
+  description = 'Performing ETL using Machine Learning',
+  author = 'Mithesh R, Arth Akhouri, Heetansh Jhaveri, Ayaan Khan',
+  author_email = 'arthakhouri@gmail.com'
+  url = 'https://github.com/user/FlintyTub49',
+  download_url = 'https://github.com/FlintyTub49/MAHA/archive/v_0.1.tar.gz',
+  keywords = ['ETL', 'Machine Learning', 'Regression', 'Pandas', 'Numpy'],
+  install_requires=[
+          'numpy',
+          'sklearn',
+          'pandas'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
+
